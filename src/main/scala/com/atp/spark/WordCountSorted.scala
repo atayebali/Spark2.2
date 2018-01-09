0b59eba2ba53656f7b419b738c76e5d1cb28d9c7
Anis Tayebali <anis.tayebali@hyfn.com>
2018-01-08 20:31:41 -0800

    Got sortByKey working when using mult cores

--- src/main/scala/com/atp/spark/WordCountSorted.scala
+++ src/main/scala/com/atp/spark/WordCountSorted.scala
@@ -15,10 +15,20 @@ object WordCountSorted {
     // Read each line of input data
     val input = sc.textFile("../SparkScala/book.txt")
     val words = input.flatMap(x => x.split("\\W+"))
-    val wordCount = words.map(x => x.toLowerCase()).map(x => (x, 1)).reduceByKey((x, y) => x + y).map(x => (x._2, x._1)).sortByKey()
-    for (res <- wordCount) {
-      val count = res._1
-      val word = res._2
+
+    // Normalize everything to lowercase
+    val lowercaseWords = words.map(x => x.toLowerCase())
+
+    // Count of the occurrences of each word
+    val wordCounts = lowercaseWords.map(x => (x, 1)).reduceByKey( (x,y) => x + y )
+
+    // Flip (word, count) tuples to (count, word) and then sort by key (the counts)
+    val wordCountsSorted = wordCounts.map( x => (x._2, x._1) ).sortByKey(numPartitions = 1)
+
+    // Print the results, flipping the (count, word) results to word: count as we go.
+    for (result <- wordCountsSorted) {
+      val count = result._1
+      val word = result._2
       println(s"$word: $count")
     }
   }
